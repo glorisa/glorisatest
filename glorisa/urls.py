fb0d378bd0496db31736c46a5102455137b3aeca
Sarthak Garg <gargsarthak30@gmail.com>
2017-05-17 11:25:09 +0530

    Removed static files

--- glorisa/urls.py
+++ glorisa/urls.py
@@ -7,7 +7,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('main.urls'))
 ]
-
-if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
-
